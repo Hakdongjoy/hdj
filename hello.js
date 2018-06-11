d09c8ee89bd8f4a1fe49b6afe5b9bacb8acad5fc
Hakdongjoy <40167370+Hakdongjoy@users.noreply.github.com>
2018-06-11 21:49:41 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,5 +4,6 @@ http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.write("Hello\r\n");
     res.write("World\r\n");
+    res.write("은성아 분발하자..!\r\n");
     res.end();
 }).listen(22);
